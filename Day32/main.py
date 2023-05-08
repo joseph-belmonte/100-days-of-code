29d303090673f3e35f6ab54ec71d65eac3105428
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-05-08 18:00:10 -0400

    Day 32 exercises

--- Day32/main.py
+++ Day32/main.py
@@ -1,4 +1,4 @@
-''' Automated Birthday Wisher'''
+""" Automated Birthday Wisher"""
 
 import os
 import smtplib
@@ -22,9 +22,15 @@ birthdays_dict = {
     (data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()
 }
 
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path, encoding="utf-8") as file:
+        contents = file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
 # select a random quote from quotes.txt
-with open("quotes.txt", encoding='utf-8') as file:
+with open("quotes.txt", encoding="utf-8") as file:
     quotes = file.readlines()
     random_quote = random.choice(quotes)
 
@@ -32,6 +38,8 @@ with open("quotes.txt", encoding='utf-8') as file:
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=MY_EMAIL, password=PASSWORD)  # type: ignore
-    connection.sendmail(from_addr=MY_EMAIL,  # type: ignore
-                        to_addrs=TO_EMAIL,  # type: ignore
-                        msg=f'Subject:Motivational Quote\n\n{random_quote}')
+    connection.sendmail(
+        from_addr=MY_EMAIL,  # type: ignore
+        to_addrs=TO_EMAIL,  # type: ignore
+        msg=f"Subject:Motivational Quote\n\n{random_quote}",
+    )
